e7ee8e411ab67d54a34df7ad04fcb88bc1fc67af
jluchuang <jlu_chuang@163.com>
2017-04-02 19:34:33 +0800

    Add twins lock
    N/A
    
    Signed-off-by: jluchuang <jlu_chuang@163.com>

---
+++ src/main/java/book/artofjavaconcurrency/locks/TwinsLockTest.java
@@ -0,0 +1,52 @@
+package book.artofjavaconcurrency.locks;
+
+import javafx.concurrent.Worker;
+import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * Created by chuang on 2017/4/2.
+ */
+public class TwinsLockTest {
+    @Test
+    public void test() {
+        final Lock lock = new TwinsLock();
+
+        class Worker extends Thread {
+            public void run() {
+                while (true) {
+                    lock.lock();
+
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        System.out.println(Thread.currentThread().getName());
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i ++) {
+            Worker w = new Worker();
+            w.setDaemon(true);
+            w.start();
+        }
+
+        for (int i = 0; i < 10; i ++) {
+            try {
+                TimeUnit.SECONDS.sleep(1);
+                System.out.println();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
